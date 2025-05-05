821afd4f888d638830649ca072506e0f6a32a7ef
PineappleLord2020 <mul20002@byui.edu>
2025-05-05 09:23:30 -0600

    I think I messed up. Changed too many things and I'm lost

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Contact } from '../contact.model';
+import { ContactItemComponent } from '../contact-item/contact-item.component';
 
 @Component({
   selector: 'app-contact-list',
@@ -9,10 +9,7 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent implements OnInit {
-  contacts: Contact[] = [
-    new Contact(1, 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', null),
-    new Contact(2, "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg", null)
-  ];
+  ContactItemComponent
 
   constructor() { }
 
