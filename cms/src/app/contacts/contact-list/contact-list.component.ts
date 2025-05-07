cd97831b363b21f8267f12635b1bfd55273118b6
Timothy Harris <timothy.harris@kikoda.com>
2025-05-07 07:09:06 -0400

    fix: resolve import issue with images in assests folder

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -6,22 +6,35 @@ import { Contact } from '../contact.model';
   selector: 'app-contact-list',
   standalone: false,
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
 export class ContactListComponent implements OnInit {
   @Output() selectedContactEvent = new EventEmitter<Contact>();
-  
+
   contacts = [
-    {id: 1, name: "R. Kent Jackson", email: "jacksonk@byui.edu", phone: "208-496-3771", imageUrl: "../../assets/images/jacksonk.jpg", group: null}, 
-    {id: 2, name: "Rex Barzee", email: "barzeer@byui.edu", phone: "208-496-3768", imageUrl: "../../assets/images/barzeer.jpg", group: null}];
+    {
+      id: 1,
+      name: 'R. Kent Jackson',
+      email: 'jacksonk@byui.edu',
+      phone: '208-496-3771',
+      imageUrl: 'images/jacksonk.jpg',
+      group: null,
+    },
+    {
+      id: 2,
+      name: 'Rex Barzee',
+      email: 'barzeer@byui.edu',
+      phone: '208-496-3768',
+      imageUrl: 'images/barzeer.jpg',
+      group: null,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  onSelected(contact: Contact){
+  onSelected(contact: Contact) {
     this.selectedContactEvent.emit(contact);
   }
 }
