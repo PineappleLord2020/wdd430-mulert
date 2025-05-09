ac9d21fdd83f22000bf149f5199bcd35a0377c72
PineappleLord2020 <mul20002@byui.edu>
2025-05-09 15:35:25 -0600

    I think this is everything for now

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Message } from '../message.model'
 
 @Component({
   selector: 'app-message-edit',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  currentSender: string = 'Andrew Mulert';
+  @ViewChild('subject') subjectInput: ElementRef;
+  @ViewChild('msgText') msgTextInput: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
+  
+  onSendMessage(){
+    const subjectValue = this.subjectInput.nativeElement.value;
+    const msgTextValue = this.msgTextInput.nativeElement.value;
+    const newMessage = new Message(
+      Math.random(),
+      this.currentSender,
+      subjectValue,
+      msgTextValue
+    );
+    this.addMessageEvent.emit(newMessage);
+    this.onClear();
+  }
+
+  onClear() {
+    this.subjectInput.nativeElement.value = '';
+    this.msgTextInput.nativeElement.value = '';
+  }
 }
--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -7,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
+
+  messages = [
+    {
+      subject: 'Can I go home please?',
+      msgText: 'It has been 20 years. Guys? Anyone?',
+      sender: 'Odysseus',
+    },
+    {
+      subject: 'Told you so',
+      msgText: 'You messed up. I gave you a warning',
+      sender: 'Athena',
+    },
+    {
+      subject: 'LOL, Idiot',
+      msgText: 'Totally deserves to be punished ngl.',
+      sender: 'Zeus',
+    },
+  ];
+
+  constructor() {}
+  
+    ngOnInit() {}
+  
+    onAddMessage(message: Message) {
+      this.messages.push(message);
+    }
 }
