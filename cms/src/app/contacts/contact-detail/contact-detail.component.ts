f8af3228ff43cd4fb49d73e7e66426cafa41a7a3
PineappleLord2020 <mul20002@byui.edu>
2025-05-30 15:27:05 -0600

    Is this the end?

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Contact } from '../contact.model'
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-detail',
@@ -8,6 +10,29 @@ import { Contact } from '../contact.model'
   templateUrl: './contact-detail.component.html',
   styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
-  @Input() contact:Contact;
+export class ContactDetailComponent implements OnInit {
+  nativeWindow: any;
+  contact: Contact;
+
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params) => {
+        const id = params['id'];
+        this.contact = this.contactService.getContact(id);
+      }
+    );
+  }
+
+  onDelete() {
+    this.contactService.deleteContact(this.contact);
+    this.router.navigateByUrl('/contacts');
+  }
 }
