fb051f1475d062d71245f5b675c2018e5493849c
PineappleLord2020 <mul20002@byui.edu>
2025-06-09 16:57:43 -0600

    Won't even start up in its current state

--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,6 +9,46 @@ import { Component } from '@angular/core';
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
 
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
+
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(){
+    this.route.params.subscribe (
+      (params: Params) => {
+        const id = params["id"];
+        if (id === undefined || id === null){
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(id);
+
+        if (this.originalContact === undefined || this.originalContact === null){
+          return
+        }
+
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+        if (this.contact.group) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+        }
+      }
+    )
+  }
+
+  onSubmit(){
+    
+  }
 }
--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DocumentsService } from '../documents.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,6 +9,46 @@ import { Component } from '@angular/core';
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
 
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+
+  constructor(
+    private documentService: DocumentsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(){
+    this.route.params.subscribe (
+      (params: Params) => {
+        const id = params['id'];
+        if (id === null || id === undefined){
+          this.editMode = false;
+          return;
+        }
+
+        this.originalDocument = this.documentService.getDocument(id);
+
+        if (this.originalDocument === null || this.originalDocument === undefined){
+          return;
+        }
+
+        this.editMode = true;
+
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      }
+    )
+  }
+
+  onSubmit(){
+    this.valve = form.valve;
+    this.newDocument = new Document();
+  }
+
+  onCancel(){
+    this.router.navigate(['/documents']);
+  }
 }
