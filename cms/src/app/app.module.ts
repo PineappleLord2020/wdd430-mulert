ed08fb1eb25091c1cdd97779563f5af242f6c696
PineappleLord2020 <mul20002@byui.edu>
2025-06-11 09:00:56 -0600

    Something new, don't quite know how to connect it in

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 import { FormsModule } from '@angular/forms';
-import { DndModule } from 'ng2-dnd';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -47,7 +47,7 @@ import { DndModule } from 'ng2-dnd';
     DropdownDirective,
     AppRoutingModule,
     FormsModule,
-    DndModule.forRoot(),
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
